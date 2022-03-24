0e34bc37a30df30e292e4a0b69a0685d2eb6bf52
kirtidalave <dalavekirti@gmail.com>
2022-03-24 16:56:22 +0530

    Exception

---
+++ Divisible.py
@@ -0,0 +1,23 @@
+import unittest
+
+def checkDivisibleby7(x):
+    if x%7 ==0:
+        return True
+    else:
+        return False
+
+class CheckDivisible(unittest.TestCase):
+
+    def test_case_divisble(self):
+            x=14
+            result = checkDivisibleby7(x)
+            self.assertTrue(result)
+
+    def test_case_divisble1(self):
+                x=9
+                result = checkDivisibleby7(x)
+                self.assertFalse(result)
+
+if __name__ == "__main__":
+    unittest.main()
+
