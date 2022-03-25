ed582cd7b87e3f69e900bccde2a70eb5cc0d31e4
kirtidalave <dalavekirti@gmail.com>
2022-03-25 11:41:31 +0530

    Even0rOdd sucessfully

--- Unittest.py
+++ Unittest.py
@@ -1,19 +1,17 @@
 import unittest
+from distutils.command.check import check
+from unittest import result
 
-def add(x,y):
-    return x+y
-class MyApp2(unittest.TestCase):
+class EvenorOddApp(unittest.TestCase):
+   def test_case_even_check(self):
+       x=10
+       reult = check(x)
+       self.assertEqual("even",result)
 
-    def test_case1(self):
-        a=10
-        b=22
-        result = add(a,b)
-        self.assertEqual(a+b, result)
-
-        def test_case2(self):
-            a = 12.5
-            b = 12.9
-            result = add(a,b)
+       def test_case_odd_check(self):
+           x = 15
+           result = check(x)
+           self.assertEqual("odd",result)
 
 if __name__ == "__main__":
     unittest.main()
